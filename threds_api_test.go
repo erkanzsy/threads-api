e6f9c31bf8ebc449eb40381bc9e7796fd9838508
Erkan Ozsoy <erkan.ozsoy@bil.omu.edu.tr>
2023-07-06 20:42:10 +0300

    zuck unittests updated

--- threds_api_test.go
+++ threds_api_test.go
@@ -6,25 +6,25 @@ import (
 )
 
 func TestUserIDFromUsername_ShouldReturnNoError(t *testing.T) {
-	userId, _ := UserIDFromUsername("erkno_")
+	userId, _ := UserIDFromUsername("zuck")
 
-	if "51180116931" != userId {
+	if "314216" != userId {
 		t.Errorf(fmt.Sprintf("userId: %s", userId))
 	}
 }
 
 func TestUserProfile_ShouldReturnNoError(t *testing.T) {
-	userProfile, _ := UserProfile("erkno_", "51180116931")
+	userProfile, _ := UserProfile("zuck", "314216")
 
-	if "erkno_" != userProfile.Data.UserData.User.Username {
+	if "zuck" != userProfile.Data.UserData.User.Username {
 		t.Errorf(fmt.Sprintf("username: %s", userProfile.Data.UserData.User.Username))
 	}
 }
 
 func TestUserThreads_ShouldReturnNoError(t *testing.T) {
-	threads, _ := UserThreads("_junhoyeo", "5438123050")
+	threads, _ := UserThreads("zuck", "314216")
 
-	if "_junhoyeo" != threads.Data.MediaData.Threads[0].ThreadItems[0].Post.User.Username {
+	if "zuck" != threads.Data.MediaData.Threads[0].ThreadItems[0].Post.User.Username {
 		t.Errorf(fmt.Sprintf("username: %s", threads.Data.MediaData.Threads[0].ThreadItems[0].Post.User.Username))
 	}
 }
