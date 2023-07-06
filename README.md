0d20056c9757f0ed0e6a0f77654559e19687e468
Erkan Ozsoy <erkan.ozsoy@bil.omu.edu.tr>
2023-07-06 21:22:59 +0300

    README.md updated for Unittests

--- README.md
+++ README.md
@@ -11,17 +11,26 @@ package main
 
 import (
 	"fmt"
-	threads_api "github.com/erkanzsy/threads-api"
+	threadsapi "github.com/erkanzsy/threads-api"
 )
 
 func main() {
-	userId, _ := threads_api.UserIDFromUsername("zuck")
+	userId, err := threadsapi.UserIDFromUsername("zuck")
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(userId) // 314216
 
-	profile, _ := threads_api.UserProfile("zuck", "314216")
+	profile, err := threadsapi.UserProfile("zuck", "314216")
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(profile.Data.UserData.User.Username) // zuck
 
-	threads, _ := threads_api.UserThreads("zuck", "314216")
+	threads, err := threadsapi.UserThreads("zuck", "314216")
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(threads.Data.MediaData.Threads[0].ThreadItems[0].Post.User.Username) // zuck
 }
 ```
--- threads.go
+++ threads.go
@@ -1,36 +1,5 @@
 package threads_api
 
-type UserProfileData struct {
-	Data struct {
-		UserData struct {
-			User struct {
-				IsPrivate            bool   `json:"is_private"`
-				ProfilePicURL        string `json:"profile_pic_url"`
-				Username             string `json:"username"`
-				HdProfilePicVersions []struct {
-					Height int    `json:"height"`
-					URL    string `json:"url"`
-					Width  int    `json:"width"`
-				} `json:"hd_profile_pic_versions"`
-				IsVerified                  bool   `json:"is_verified"`
-				Biography                   string `json:"biography"`
-				BiographyWithEntities       any    `json:"biography_with_entities"`
-				FollowerCount               int    `json:"follower_count"`
-				ProfileContextFacepileUsers any    `json:"profile_context_facepile_users"`
-				BioLinks                    []struct {
-					URL string `json:"url"`
-				} `json:"bio_links"`
-				Pk       string `json:"pk"`
-				FullName string `json:"full_name"`
-				ID       any    `json:"id"`
-			} `json:"user"`
-		} `json:"userData"`
-	} `json:"data"`
-	Extensions struct {
-		IsFinal bool `json:"is_final"`
-	} `json:"extensions"`
-}
-
 type UserThreadsData struct {
 	Data struct {
 		MediaData struct {
